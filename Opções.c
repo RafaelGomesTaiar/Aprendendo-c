0c56ff179754abb3cf82c4f886aeb7fed498b669
Rafael Gomes Taiar <taiar.rafael76@gmail.com>
2025-09-01 16:05:23 -0300

    Add files via upload

---
+++ Opções.c
@@ -0,0 +1,75 @@
+/*(Adaptado de ASCENCIO e CAMPOS, 2008) Faça um programa que receba quatro 
+valores, I, A, B e C. I é um valor inteiro e positivo e A, B e C são valores reais e distintos. 
+Escreva os números A, B e C obedecendo à tabela a seguir. Supor que o valor digitado 
+para I seja sempre um valor válido, ou seja, 1, 2 ou 3.*/
+
+#include <stdio.h> 
+int main(){ 
+    float A, B, C; 
+    int I; 
+    printf("Insira o primeiro numero (A):\n"); 
+    scanf("%f", &A); 
+    printf("Insira o segundo numero (B):\n"); 
+    scanf("%f", &B); 
+    printf("Insira o terceiro numero (C):\n"); 
+    scanf("%f", &C); 
+    printf("Insira a opcao (I-1=ORDEM CRESCENTE, 2=ORDEM DECRESCENTE, 3=O MAIOR FICA ENTRE OS DOIS):\n"); 
+    scanf("%d", &I); 
+    if (I == 1){ 
+        if((A < B) && (A < C)){ 
+            if(B < C){ 
+                printf("%f - %f - %f.\n", A, B, C); 
+            } else { 
+                printf("%f - %f - %f.\n", A, C, B); 
+            } 
+        } 
+        if((B < A) && (B < C)){ 
+            if(A < C){ 
+                printf("%f - %f - %f.\n", B, A, C); 
+            } else { 
+                printf("%f - %f - %f.\n", B, C, A); 
+            } 
+        } 
+        if((C < A) && (C < B)){ 
+            if(A < B){ 
+                printf("%f - %f - %f.\n", C, A, B); 
+            } else { 
+                printf("%f - %f - %f.\n", C, B, A); 
+            } 
+        } 
+    } 
+    if (I == 2){ 
+        if((A > B) && (A > C)){ 
+            if(B > C){ 
+                printf("%f - %f - %f.\n", A, B, C); 
+            } else { 
+                printf("%f - %f - %f.\n", A, C, B); 
+            } 
+        } 
+        if((B > A) && (B > C)){ 
+            if(A > C){ 
+                printf("%f - %f - %f.\n", B, A, C); 
+            } else { 
+                printf("%f - %f - %f.\n", B, C, A);
+                       } 
+        } 
+        if((C > A) && (C > B)){ 
+            if(A > B){ 
+                printf("%f - %f - %f.\n", C, A, B); 
+            } else { 
+                printf("%f - %f - %f.\n", C, B, A); 
+            } 
+        } 
+    } 
+    if(I == 3){ 
+        if ((A > B) && (A > C)){ 
+            printf("%f - %f - %f.\n", B, A, C); 
+        } 
+        if ((B > A) && (B > C)){ 
+            printf("%f - %f - %f.\n", A, B, C); 
+        } 
+        if ((C > A) && (C > B)){ 
+            printf("%f - %f - %f.\n", A, C, B); 
+        } 
+    } 
+} 
